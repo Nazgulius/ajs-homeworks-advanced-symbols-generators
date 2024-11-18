d8e5109eb940c3af66a176bae7b7b4f2d903cc89
Maksim Chalyh <nazgulius@yandex.ru>
2024-11-18 11:36:54 +0300

    fix

--- src/js/Team.js
+++ src/js/Team.js
@@ -7,12 +7,6 @@ export default class Team {
     this.charectors.push(character);  
   }
 
-  teamList() {
-    for (const value of this.charectors) {
-      console.log(value);
-    }
-  }
-
   *[Symbol.iterator]() {  
     for (let i = 0; i < this.charectors.length; i++) {  
       yield this.charectors[i];  
